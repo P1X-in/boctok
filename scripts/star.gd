508d35e0c9e4f779fd3e268b1b20a6bc8bfeac69
Krzysztof Jankowski <w84death@gmail.com>
2017-01-22 00:06:33 +0100

    more galaxies, better probability

--- scripts/star.gd
+++ scripts/star.gd
@@ -2,15 +2,16 @@ var anim
 var list
 var random_animation
 var SEED = 0.777
+var RAND_STARS = 100
+var RAND_GALAXIES = 15
 
 func _ready():
-	randomize()
 	rand_seed(self.SEED)
 	self.anim = self.get_node("anim")
 	self.list = self.anim.get_animation_list()
-	if randi()%100<75:
+	if randi()%100 < self.RAND_STARS:
 		self.random_animation = self.list[int(rand_range(1,4))]
-		if randi()%100<15:
+		if randi()%100<self.RAND_GALAXIES:
 			self.random_animation = self.list[int(rand_range(4,9))]
 		self.anim.play(self.random_animation)
 		self.anim.seek(randf()*self.anim.get_current_animation_length())
