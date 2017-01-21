26d888ff69cb2015bcbe5898f126f2b8b5b06d7e
Krzysztof Jankowski <w84death@gmail.com>
2017-01-21 03:52:19 +0100

    awesome intro scene

---
+++ scripts/star.tres.gd
@@ -0,0 +1,14 @@
+var anim
+var list
+var random_animation
+
+func _ready():
+	randomize()
+	self.anim = self.get_node("anim")
+	self.list = self.anim.get_animation_list()
+	if randi()%100<75:
+		self.random_animation = self.list[int(rand_range(1,4))]
+		if randi()%100<15:
+			self.random_animation = self.list[int(rand_range(5,9))]
+		self.anim.play(self.random_animation)
+		self.anim.seek(randf()*self.anim.get_current_animation_length())
