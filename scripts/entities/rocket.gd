82af87feb5fe814637b948aebf9c012e0f19f3ec
CzlowiekImadlo <quak@tlen.pl>
2017-01-21 21:32:03 +0100

    Rockets have thrust

--- scripts/entities/rocket.gd
+++ scripts/entities/rocket.gd
@@ -8,6 +8,9 @@ var current_gravity = Vector2(0, 0)
 var initial_velocity = Vector2(0, 0)
 var started = false
 
+var engines = []
+
+
 func _integrate_forces(s):
     var pos = self.get_pos()
 
@@ -16,6 +19,8 @@ func _integrate_forces(s):
 
     var lv = s.get_linear_velocity()
     var step = s.get_step()
+
+    var engines_on = {"Main" : true, "Left": false, "Right" : false}
 
     self.set_rot(lv.angle() + 3.14)
 
@@ -28,10 +33,22 @@ func _integrate_forces(s):
         self.started = true
 
     s.set_linear_velocity(lv)
+    self.__engines_start(engines_on)
 
 func _colliding_body(body):
     print('boom')
     self.queue_free()
 
+func __engines_start(engines):
+    for engine in engines:
+        if engines[engine] == true:
+            self.engines[engine].set_emitting(true)
+
 func _ready():
     set_fixed_process(true)
+    var engines = self.get_node("Engines")
+    self.engines = {
+        "Main" : engines.get_node("Main"),
+        "Left" : engines.get_node("Left"),
+        "Right" : engines.get_node("Right"),
+    }
